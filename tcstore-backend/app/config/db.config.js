c2ba680dd46ab3b4e76323c240abeab14d7cc870
TrieuVD <trieuvd@runsystem.net>
2023-12-05 00:02:26 +0700

    config db server

--- tcstore-backend/app/config/db.config.js
+++ tcstore-backend/app/config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    URL: process.env.MONGODB_URI || "mongodb://localhost:27017/dastore?retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000"
-}
+  URL:
+    process.env.MONGODB_URI ||
+    "mongodb://root:passw0rd@vuductrieu.id.vn:27018/dastore?authSource=admin&retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
+};
