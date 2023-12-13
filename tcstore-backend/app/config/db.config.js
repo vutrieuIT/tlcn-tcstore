92b1a0050317ba3fb79f915d8cd49b87b505f83d
TrieuVD <trieuvd@runsystem.net>
2023-12-13 21:37:07 +0700

    reset

--- tcstore-backend/app/config/db.config.js
+++ tcstore-backend/app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   URL:
     process.env.MONGODB_URI ||
-    "mongodb://root:passw0rd@vuductrieu.id.vn:27018/dastore?authSource=admin&retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
+    "mongodb://root:passw0rd@vuductrieu.id.vn:27018/tcstore?authSource=admin&retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
 };
