e1d12d544d9efb29a17106a21f86c614207769ec
TrieuVD <trieuvd@runsystem.net>
2023-12-13 22:15:26 +0700

    remove uri

--- tcstore-backend/app/config/db.config.js
+++ tcstore-backend/app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   URL:
     process.env.MONGODB_URI ||
-    "mongodb://root:passw0rd@vuductrieu.id.vn:27018/tcstore?authSource=admin&retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
+    "mongodb://localhost:27017/tcstore?authSource=admin&retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
 };
