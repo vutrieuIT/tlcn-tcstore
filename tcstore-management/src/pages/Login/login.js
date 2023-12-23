107b4527a3b775feb9be40295d26e61012c48ba7
TrieuVD <trieuvd@runsystem.net>
2023-12-23 20:23:24 +0700

    update UI

--- tcstore-management/src/pages/Login/login.js
+++ tcstore-management/src/pages/Login/login.js
@@ -64,7 +64,7 @@ const Login = () => {
             onFinish={onFinish}
           >
             <Form.Item style={{ marginBottom: 3, marginTop: 65 }}>
-              <Divider style={{ marginBottom: 5, fontSize: 19 }} orientation="center">CHÀO MỪNG BẠN ĐẾN VỚI DASTORE!</Divider>
+              <Divider style={{ marginBottom: 5, fontSize: 19 }} orientation="center">CHÀO MỪNG BẠN ĐẾN VỚI TCSTORE!</Divider>
             </Form.Item>
             <Form.Item style={{ marginBottom: 16, textAlign: "center" }}>
               <p className="text">Đăng nhập để vào hệ thống quản lý</p>
