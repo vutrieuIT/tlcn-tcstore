1dbc6d3112a0cb608002c016dd29ac251b0dc57a
TrieuVD <trieuvd@runsystem.net>
2023-12-23 20:19:22 +0700

    update news

--- tcstore-backend/app/models/news.js
+++ tcstore-backend/app/models/news.js
@@ -1,21 +1,27 @@
-const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const NewsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const NewsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      default:
+        "https://pixabay.com/illustrations/news-headlines-newsletter-636978/",
+      required: true,
+    },
   },
-  description:{
-    type: String,
-    default: ''
-  },
-  image: {
-    type: String,
-    required: true
-  },
-}, { timestamps: true }, { collection: 'News' });
+  { timestamps: true },
+  { collection: "News" }
+);
 
 NewsSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model('News', NewsSchema);
+module.exports = mongoose.model("News", NewsSchema);
