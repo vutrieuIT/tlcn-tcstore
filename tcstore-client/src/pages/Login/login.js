ebfc25903347bad72fe2c9c2caac438b6fc2b846
TrieuVD <trieuvd@runsystem.net>
2023-12-23 09:48:22 +0700

    update page login

--- tcstore-client/src/pages/Login/login.js
+++ tcstore-client/src/pages/Login/login.js
@@ -79,14 +79,14 @@ const Login = () => {
                 rules={[
                   {
                     required: true,
-                    message: 'Please input your Username!',
+                    message: 'Please input your email!',
                   },
                 ]}
               >
                 <Input
                   style={{ height: 34, borderRadius: 5 }}
                   prefix={<UserOutlined className="siteformitemicon" />}
-                  placeholder="Username" />
+                  placeholder="Email" />
               </Form.Item >
               <Form.Item
                 style={{ marginBottom: 8 }}
