ed55672b8de0eced0f735daad3b05797649abc43
TrieuVD <trieuvd@runsystem.net>
2023-12-31 21:12:32 +0700

    fix warning mng/dashBoard.js

--- tcstore-management/src/pages/DashBoard/dashBoard.js
+++ tcstore-management/src/pages/DashBoard/dashBoard.js
@@ -3,73 +3,30 @@ import "./dashBoard.css";
 import {
   Col,
   Row,
-  Typography,
   Spin,
-  Button,
-  PageHeader,
   Card,
-  Badge,
-  Empty,
-  Input,
-  Space,
-  Form,
-  Pagination,
-  Modal,
-  Popconfirm,
-  notification,
   BackTop,
-  Tag,
   Breadcrumb,
-  Select,
-  Table,
 } from "antd";
 import {
-  AppstoreAddOutlined,
   DashboardOutlined,
-  DeleteOutlined,
-  PlusOutlined,
-  EyeOutlined,
-  ExclamationCircleOutlined,
-  SearchOutlined,
-  CalendarOutlined,
-  UserOutlined,
-  TeamOutlined,
   HomeOutlined,
   ShopTwoTone,
   ContactsTwoTone,
   HddTwoTone,
   ShoppingTwoTone,
-  EditOutlined,
 } from "@ant-design/icons";
 import statisticApi from "../../apis/statistic";
-import { useHistory } from "react-router-dom";
-import { DateTime } from "../../utils/dateTime";
-import axiosClient from "../../apis/axiosClient";
-
-const { confirm } = Modal;
-const DATE_TIME_FORMAT = "DD/MM/YYYY HH:mm";
-const { Title } = Typography;
 
 const DashBoard = () => {
   const [statisticList, setStatisticList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [total, setTotalList] = useState();
-  const [id, setId] = useState();
-
-  const history = useHistory();
-
-  function NoData() {
-    return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
-  }
 
   useEffect(() => {
     (async () => {
       try {
         await statisticApi.getTotal().then((res) => {
           console.log(res);
-          setTotalList(res);
           setStatisticList(res.data);
-          setLoading(false);
         });
       } catch (error) {
         console.log("Failed to fetch event list:" + error);
